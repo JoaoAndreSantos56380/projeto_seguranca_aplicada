63253343f37efa926de4a7b4861b281d2476577c
Rafael Ferreira <rafaelmmf19062000@gmail.com>
2025-03-31 19:43:07 +0100

    Minor refactor for simplicity

--- Account.java
+++ Account.java
@@ -1,36 +1,34 @@
-public class Accounts {
-    private  String Name;
+public class Account {
+    private String Name;
     private String cardFile;
     private Double balance;
 
     public void setName (String Name){
-        this.Name=Name;
+        this.Name = Name;
     }
 
     public void setCardFile (String cardFile){
-        this.cardFile=cardFile;
+        this.cardFile = cardFile;
     }
 
-    public void setBalance (Double balance){
-        this.balance=balance;
+    public void setBalance (Double value){
+        this.balance = value;
     }
 
-    public void addBalance (Double addition){
-        this.balance += addition;
+    public void addBalance (Double value){
+        this.balance += value;
     }
 
-    public void LessBalance (Double less){
-        this.balance -= less;
+    public void subBalance(Double value){
+        this.balance -= value;
     }
 
     public double getBalance (){
         return this.balance;
     }
-    public boolean VerifyAccount() {
-        if (this.Name==null || this.cardFile==null || this.balance==null)
-            return false;
-        else
-            return true;
+
+    public boolean verifyAccount() {
+        return this.Name != null && this.cardFile != null && this.balance != null;
     }
 
     public String toJson() {
